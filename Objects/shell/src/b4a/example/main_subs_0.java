10b1784eccebe26395760c2b4ba80987e8bf4878
behrooz <b.yazdaani@gmail.com>
2022-01-25 23:24:14 +0330

    Start First App

---
+++ Objects/shell/src/b4a/example/main_subs_0.java
@@ -0,0 +1,108 @@
+package b4a.example;
+
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.pc.*;
+
+public class main_subs_0 {
+
+
+public static RemoteObject  _activity_create(RemoteObject _firsttime) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Create (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,25);
+if (RapidSub.canDelegate("activity_create")) { return b4a.example.main.remoteMe.runUserSub(false, "main","activity_create", _firsttime);}
+Debug.locals.put("FirstTime", _firsttime);
+ BA.debugLineNum = 25;BA.debugLine="Sub Activity_Create(FirstTime As Boolean)";
+Debug.ShouldStop(16777216);
+ BA.debugLineNum = 26;BA.debugLine="Activity.LoadLayout(\"Layout\")";
+Debug.ShouldStop(33554432);
+main.mostCurrent._activity.runMethodAndSync(false,"LoadLayout",(Object)(RemoteObject.createImmutable("Layout")),main.mostCurrent.activityBA);
+ BA.debugLineNum = 27;BA.debugLine="End Sub";
+Debug.ShouldStop(67108864);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_pause(RemoteObject _userclosed) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Pause (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,33);
+if (RapidSub.canDelegate("activity_pause")) { return b4a.example.main.remoteMe.runUserSub(false, "main","activity_pause", _userclosed);}
+Debug.locals.put("UserClosed", _userclosed);
+ BA.debugLineNum = 33;BA.debugLine="Sub Activity_Pause (UserClosed As Boolean)";
+Debug.ShouldStop(1);
+ BA.debugLineNum = 35;BA.debugLine="End Sub";
+Debug.ShouldStop(4);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_resume() throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Resume (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,29);
+if (RapidSub.canDelegate("activity_resume")) { return b4a.example.main.remoteMe.runUserSub(false, "main","activity_resume");}
+ BA.debugLineNum = 29;BA.debugLine="Sub Activity_Resume";
+Debug.ShouldStop(268435456);
+ BA.debugLineNum = 31;BA.debugLine="End Sub";
+Debug.ShouldStop(1073741824);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _button1_click() throws Exception{
+try {
+		Debug.PushSubsStack("Button1_Click (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,37);
+if (RapidSub.canDelegate("button1_click")) { return b4a.example.main.remoteMe.runUserSub(false, "main","button1_click");}
+ BA.debugLineNum = 37;BA.debugLine="Sub Button1_Click";
+Debug.ShouldStop(16);
+ BA.debugLineNum = 38;BA.debugLine="xui.MsgboxAsync(\"Hello world!\", \"B4X\")";
+Debug.ShouldStop(32);
+main._xui.runVoidMethod ("MsgboxAsync",main.processBA,(Object)(BA.ObjectToCharSequence("Hello world!")),(Object)(BA.ObjectToCharSequence(RemoteObject.createImmutable("B4X"))));
+ BA.debugLineNum = 39;BA.debugLine="End Sub";
+Debug.ShouldStop(64);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			throw Debug.ErrorCaught(e);
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _globals() throws Exception{
+ //BA.debugLineNum = 21;BA.debugLine="Sub Globals";
+ //BA.debugLineNum = 23;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+
+public static void initializeProcessGlobals() {
+    
+    if (main.processGlobalsRun == false) {
+	    main.processGlobalsRun = true;
+		try {
+		        main_subs_0._process_globals();
+starter_subs_0._process_globals();
+main.myClass = BA.getDeviceClass ("b4a.example.main");
+starter.myClass = BA.getDeviceClass ("b4a.example.starter");
+		
+        } catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+    }
+}public static RemoteObject  _process_globals() throws Exception{
+ //BA.debugLineNum = 15;BA.debugLine="Sub Process_Globals";
+ //BA.debugLineNum = 18;BA.debugLine="Private xui As XUI";
+main._xui = RemoteObject.createNew ("anywheresoftware.b4a.objects.B4XViewWrapper.XUI");
+ //BA.debugLineNum = 19;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+}
---
+++ Objects/src/b4a/example/starter.java
@@ -0,0 +1,186 @@
+package b4a.example;
+
+
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.objects.ServiceHelper;
+import anywheresoftware.b4a.debug.*;
+
+public class starter extends  android.app.Service{
+	public static class starter_BR extends android.content.BroadcastReceiver {
+
+		@Override
+		public void onReceive(android.content.Context context, android.content.Intent intent) {
+            BA.LogInfo("** Receiver (starter) OnReceive **");
+			android.content.Intent in = new android.content.Intent(context, starter.class);
+			if (intent != null)
+				in.putExtra("b4a_internal_intent", intent);
+            ServiceHelper.StarterHelper.startServiceFromReceiver (context, in, true, anywheresoftware.b4a.ShellBA.class);
+		}
+
+	}
+    static starter mostCurrent;
+	public static BA processBA;
+    private ServiceHelper _service;
+    public static Class<?> getObject() {
+		return starter.class;
+	}
+	@Override
+	public void onCreate() {
+        super.onCreate();
+        mostCurrent = this;
+        if (processBA == null) {
+		    processBA = new anywheresoftware.b4a.ShellBA(this, null, null, "b4a.example", "b4a.example.starter");
+            if (BA.isShellModeRuntimeCheck(processBA)) {
+                processBA.raiseEvent2(null, true, "SHELL", false);
+		    }
+            try {
+                Class.forName(BA.applicationContext.getPackageName() + ".main").getMethod("initializeProcessGlobals").invoke(null, null);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            processBA.loadHtSubs(this.getClass());
+            ServiceHelper.init();
+        }
+        _service = new ServiceHelper(this);
+        processBA.service = this;
+        
+        if (BA.isShellModeRuntimeCheck(processBA)) {
+			processBA.raiseEvent2(null, true, "CREATE", true, "b4a.example.starter", processBA, _service, anywheresoftware.b4a.keywords.Common.Density);
+		}
+        if (!true && ServiceHelper.StarterHelper.startFromServiceCreate(processBA, false) == false) {
+				
+		}
+		else {
+            processBA.setActivityPaused(false);
+            BA.LogInfo("*** Service (starter) Create ***");
+            processBA.raiseEvent(null, "service_create");
+        }
+        processBA.runHook("oncreate", this, null);
+        if (true) {
+			ServiceHelper.StarterHelper.runWaitForLayouts();
+		}
+    }
+		@Override
+	public void onStart(android.content.Intent intent, int startId) {
+		onStartCommand(intent, 0, 0);
+    }
+    @Override
+    public int onStartCommand(final android.content.Intent intent, int flags, int startId) {
+    	if (ServiceHelper.StarterHelper.onStartCommand(processBA, new Runnable() {
+            public void run() {
+                handleStart(intent);
+            }}))
+			;
+		else {
+			ServiceHelper.StarterHelper.addWaitForLayout (new Runnable() {
+				public void run() {
+                    processBA.setActivityPaused(false);
+                    BA.LogInfo("** Service (starter) Create **");
+                    processBA.raiseEvent(null, "service_create");
+					handleStart(intent);
+                    ServiceHelper.StarterHelper.removeWaitForLayout();
+				}
+			});
+		}
+        processBA.runHook("onstartcommand", this, new Object[] {intent, flags, startId});
+		return android.app.Service.START_NOT_STICKY;
+    }
+    public void onTaskRemoved(android.content.Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+        if (true)
+            processBA.raiseEvent(null, "service_taskremoved");
+            
+    }
+    private void handleStart(android.content.Intent intent) {
+    	BA.LogInfo("** Service (starter) Start **");
+    	java.lang.reflect.Method startEvent = processBA.htSubs.get("service_start");
+    	if (startEvent != null) {
+    		if (startEvent.getParameterTypes().length > 0) {
+    			anywheresoftware.b4a.objects.IntentWrapper iw = ServiceHelper.StarterHelper.handleStartIntent(intent, _service, processBA);
+    			processBA.raiseEvent(null, "service_start", iw);
+    		}
+    		else {
+    			processBA.raiseEvent(null, "service_start");
+    		}
+    	}
+    }
+	
+	@Override
+	public void onDestroy() {
+        super.onDestroy();
+        if (true) {
+            BA.LogInfo("** Service (starter) Destroy (ignored)**");
+        }
+        else {
+            BA.LogInfo("** Service (starter) Destroy **");
+		    processBA.raiseEvent(null, "service_destroy");
+            processBA.service = null;
+		    mostCurrent = null;
+		    processBA.setActivityPaused(true);
+            processBA.runHook("ondestroy", this, null);
+        }
+	}
+
+@Override
+	public android.os.IBinder onBind(android.content.Intent intent) {
+		return null;
+	}
+public anywheresoftware.b4a.keywords.Common __c = null;
+public b4a.example.main _main = null;
+public static boolean  _application_error(anywheresoftware.b4a.objects.B4AException _error,String _stacktrace) throws Exception{
+RDebugUtils.currentModule="starter";
+if (Debug.shouldDelegate(processBA, "application_error", false))
+	 {return ((Boolean) Debug.delegate(processBA, "application_error", new Object[] {_error,_stacktrace}));}
+RDebugUtils.currentLine=655360;
+ //BA.debugLineNum = 655360;BA.debugLine="Sub Application_Error (Error As Exception, StackTr";
+RDebugUtils.currentLine=655361;
+ //BA.debugLineNum = 655361;BA.debugLine="Return True";
+if (true) return anywheresoftware.b4a.keywords.Common.True;
+RDebugUtils.currentLine=655362;
+ //BA.debugLineNum = 655362;BA.debugLine="End Sub";
+return false;
+}
+public static String  _service_create() throws Exception{
+RDebugUtils.currentModule="starter";
+if (Debug.shouldDelegate(processBA, "service_create", false))
+	 {return ((String) Debug.delegate(processBA, "service_create", null));}
+RDebugUtils.currentLine=458752;
+ //BA.debugLineNum = 458752;BA.debugLine="Sub Service_Create";
+RDebugUtils.currentLine=458756;
+ //BA.debugLineNum = 458756;BA.debugLine="End Sub";
+return "";
+}
+public static String  _service_destroy() throws Exception{
+RDebugUtils.currentModule="starter";
+if (Debug.shouldDelegate(processBA, "service_destroy", false))
+	 {return ((String) Debug.delegate(processBA, "service_destroy", null));}
+RDebugUtils.currentLine=720896;
+ //BA.debugLineNum = 720896;BA.debugLine="Sub Service_Destroy";
+RDebugUtils.currentLine=720898;
+ //BA.debugLineNum = 720898;BA.debugLine="End Sub";
+return "";
+}
+public static String  _service_start(anywheresoftware.b4a.objects.IntentWrapper _startingintent) throws Exception{
+RDebugUtils.currentModule="starter";
+if (Debug.shouldDelegate(processBA, "service_start", false))
+	 {return ((String) Debug.delegate(processBA, "service_start", new Object[] {_startingintent}));}
+RDebugUtils.currentLine=524288;
+ //BA.debugLineNum = 524288;BA.debugLine="Sub Service_Start (StartingIntent As Intent)";
+RDebugUtils.currentLine=524289;
+ //BA.debugLineNum = 524289;BA.debugLine="Service.StopAutomaticForeground 'Starter service";
+mostCurrent._service.StopAutomaticForeground();
+RDebugUtils.currentLine=524290;
+ //BA.debugLineNum = 524290;BA.debugLine="End Sub";
+return "";
+}
+public static String  _service_taskremoved() throws Exception{
+RDebugUtils.currentModule="starter";
+if (Debug.shouldDelegate(processBA, "service_taskremoved", false))
+	 {return ((String) Debug.delegate(processBA, "service_taskremoved", null));}
+RDebugUtils.currentLine=589824;
+ //BA.debugLineNum = 589824;BA.debugLine="Sub Service_TaskRemoved";
+RDebugUtils.currentLine=589826;
+ //BA.debugLineNum = 589826;BA.debugLine="End Sub";
+return "";
+}
+}
